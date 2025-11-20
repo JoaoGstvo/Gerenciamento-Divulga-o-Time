bc12784e985cc7917b03e4dc920db8437769bc62
JoaoGstvo <joaogustavocarmomoraes@gmail.com>
2025-11-19 22:23:39 -0300

    Excluindo a rota do header

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// Components
-import Header from './Components/Header';
-
 // Landing Pages
 import LandingPageDivulgação from './Pages/LandingPages/LandingPageDivulgação';
 
@@ -12,7 +9,6 @@ export default function Index() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPageDivulgação />} />
-                <Route path="/header" element={<Header />}  />
             </Routes>
         </BrowserRouter>
     )
