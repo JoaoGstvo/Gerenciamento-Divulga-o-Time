d15913f4a4002282e1bc4b7ec32c9edfe34851c3
JoaoGstvo <joaogustavocarmomoraes@gmail.com>
2025-11-29 22:33:28 -0300

    Testes visuais no card atleta

--- src/Components/CardAtleta/index.js
+++ src/Components/CardAtleta/index.js
@@ -3,22 +3,11 @@ import "./index.scss";
 function CardAtleta() {
     return (
         <main className="cardAtleta">
-            <div id="imagem">
-
-            </div>
-
-            <h3>Nome do Atleta</h3>
-
-            <div id="informacoes">
-                <div className="informações">
-                    <p>Cat: Nome</p>
-                    <p>Posç: Atacante</p>
-                </div>
-                <div className="informações">
-                    <p>N°: 10</p>
-                    <p>Info: Info</p>
-                </div>
-            </div>
+            <div id="imagem"></div>
+            <section>
+                <h3>Nome do Atleta</h3>
+                <h1>10</h1>
+            </section>
         </main>
     );
 }
