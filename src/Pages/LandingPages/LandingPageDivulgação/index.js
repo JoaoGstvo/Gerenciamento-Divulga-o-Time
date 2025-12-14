ee562b8d10e795daa2b223095f7056fc27bf4937
JoaoGstvo <joaogustavocarmomoraes@gmail.com>
2025-12-14 19:10:33 -0300

    Parte sobre nós feita

--- src/Pages/LandingPages/LandingPageDivulgação/index.js
+++ src/Pages/LandingPages/LandingPageDivulgação/index.js
@@ -33,6 +33,32 @@ function LandingPageDivulgação() {
             </div>
           </div>
         </section>
+
+        <section className="sobreNos">
+            <div className="titulo">
+                <h1>Sobre o Projeto</h1>
+            </div>
+
+            <div className="informacoes">
+                <div className="info">
+                <div className="img"></div>   
+                    <h3>Nosso Time</h3>
+                    <p>Somos mais do que um grupo de atletas: somos uma equipe unida pelo respeito, disciplina e vontade de vencer.</p>
+                </div>
+                <div className="info">
+                    <div className="img"></div>
+                    <h3>Treinos e Eventos</h3>
+                    <p>Somos mais do que um grupo de atletas: somos uma equipe unida pelo respeito, disciplina e vontade de vencer.</p>
+                </div>
+                <div className="info">
+                    <div className="img"></div>
+                    <h3>Objetivo</h3>
+                    <p>Somos mais do que um grupo de atletas: somos uma equipe unida pelo respeito, disciplina e vontade de vencer.</p>
+                </div>
+            </div>
+
+
+        </section>
         <Footer />
     </main>
   );
